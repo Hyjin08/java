0ec86e50bbc05e993a56cbffb69d50ed6b18b5a0
Hyjin08 <daejung2610@dge.go.kr>
2026-04-03 12:18:35 +0900

    Add files via upload

---
+++ S307.java
@@ -0,0 +1,12 @@
+package jump2java;
+
+public class S307 {
+	public static void main(String[]args) {
+		String a = "Hello Java";
+		System.out.println(a.toUpperCase());
+		
+		String b = "a:b:c:d";
+		String[] result = a.split(":");
+	}
+
+}
