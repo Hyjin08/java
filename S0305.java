69caa94373b5fef35ffafecaf60493817ccfae60
Hyjin08 <daejung2610@dge.go.kr>
2026-04-03 11:29:52 +0900

    Add files via upload

---
+++ S0305.java
@@ -0,0 +1,13 @@
+package jump2java;
+
+public class S0305 {
+	public static void main(String[]args) {
+		StringBuffer sb = new StringBuffer();  
+		sb.append("hello");
+		sb.append(" ");
+		sb.append("jump to java");
+		String result = sb.toString();
+		System.out.println(result);
+	}
+
+}
