58552b22af324cafdfc02591cada0f1fe13823f1
Hyjin08 <daejung2610@dge.go.kr>
2026-04-21 10:09:59 +0900

    Add files via upload

---
+++ Test0309_2.java
@@ -0,0 +1,10 @@
+package jump2java;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Test0309_2 {
+	public static void main(String[] args) {
+		HashSet<String> set = new HashSet<>(Arrays.asList("H", "e", "1", "1", "o"));
+		System.out.println(set);
+	}
+}
