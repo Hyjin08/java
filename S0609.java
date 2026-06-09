ea87dfc5ceaed27d9394b08b42117459ea24bf98
Hyjin08 <daejung2610@dge.go.kr>
2026-06-09 10:38:03 +0900

    Add files via upload

---
+++ S0609.java
@@ -0,0 +1,46 @@
+package sample;
+import java.util.Scanner;
+
+class Book{
+	String title;
+	String author;
+	int year;
+	boolean borrow;
+	
+	Book(String title, String author, int year){
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		borrow = false;
+	}
+	void show() {
+		System.out.println("제목"+ title);
+	}
+	
+	String borrowOf() {
+		if(borrow) {
+			return "대출 중";
+		} else {
+			borrow = true;
+			return "대출 가능";
+		}
+	}
+}
+
+public class S0609 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("책 제목: ");
+		String title = sc.next();
+		System.out.println("저자: ");
+		String author = sc.next();
+		System.out.println("발행연도: ");
+		int year = sc.nextInt();
+
+		Book b1 = new Book(title, author, year);
+		b1.show();
+		System.out.println("대출 상태: "+ b1.borrowOf());
+	}
+
+}
---
+++ T0609A.java
@@ -0,0 +1,10 @@
+package jump2java;
+
+public class T0609A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
